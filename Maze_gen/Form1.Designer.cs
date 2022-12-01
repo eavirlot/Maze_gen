a2a9539bb00713f528b3bb5881998fd254de3a4c
Artem <Artem@WS-9>
2022-12-01 18:26:58 +0300

    Добавьте файлы проекта.

---
+++ Maze_gen/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿using System;
+
+namespace Maze_gen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(235, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "10";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(235, 76);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "10";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(423, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoSize = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(334, 230);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(69, 49);
+            this.panel1.TabIndex = 8;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(155, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Высота";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(155, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Ширина";
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.LargeChange = 9;
+            this.hScrollBar1.Location = new System.Drawing.Point(534, 171);
+            this.hScrollBar1.Maximum = 10;
+            this.hScrollBar1.Minimum = 2;
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(140, 24);
+            this.hScrollBar1.TabIndex = 11;
+            this.hScrollBar1.Value = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(548, 148);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 12;
+            this.textBox3.Text = "10";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(54, 117);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Maze_gen/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+
+using System.Drawing;
+
+using System.Windows.Forms;
+
+namespace Maze_gen
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            /*this.ResizeRedraw = false;
+            this.DoubleBuffered = true;*/
+        }
+
+        bool begin = false;
+        int pass = 1;
+        int wall = 0;
+        int side = 2;
+
+        int height = 0;
+        int width = 0;
+        int[,] maze;
+
+
+
+
+        public void Main()
+        {
+                                   
+        }
+    
+        void DrawBox(int x, int y, Brush br, Graphics gr)
+        {
+            gr.FillRectangle(br, x * side, y * side, side, side);
+        }
+        bool deadend(int x, int y, int[,] maze, int height, int width, int pass, int wall)
+        {
+            int a = 0;
+
+            if (x != 1)
+            {
+                if (maze[y, x - 2] == pass)
+                {
+                    a += 1;
+                }
+            }
+            else
+            {
+                a += 1;
+            }
+
+            if (y != 1)
+            {
+                if (maze[y - 2, x] == pass)
+                {
+                    a += 1;
+                }
+            }
+            else
+            {
+                a += 1;
+            }
+
+            if (x != width - 2)
+            {
+                if (maze[y, x + 2] == pass)
+                {
+                    a += 1;
+                }
+            }
+            else
+            {
+                a += 1;
+            }
+
+            if (y != height - 2)
+            {
+                if (maze[y + 2, x] == pass)
+                {
+                    a += 1;
+                }
+            }
+            else
+            {
+                a += 1;
+            }
+
+            if (a == 4)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        void mazemake(int[,] maze, int height, int width, Random r, int pass, int wall)
+        {
+            int x;
+            int y;
+            int c;
+            int a;
+
+            x = 3; y = 3; a = 0; // Точка приземления крота и счетчик
+            while (a < 1000000)   // Аварийный выход из цикла
+            {
+                maze[y, x] = pass;
+                a++;
+
+                while (true) // Бесконечный цикл, который прерывается только тупиком
+                {
+                    c = (r.Next(0, 4)); // Напоминаю, что крот прорывает по две клетки в одном направлении за прыжок
+
+                    switch (c)          // 
+                    {
+                        case 0: // Вверх
+                            if (y != 1)
+                            {
+                                if (maze[y - 2, x] == wall)
+                                {
+                                    maze[y - 1, x] = pass;
+                                    maze[y - 2, x] = pass;
+                                    y -= 2;
+                                }
+                            }
+                            break;
+
+                        case 1: // Вниз
+                            if (y != height - 2)
+                            {
+                                if (maze[y + 2, x] == wall)
+                                {
+                                    maze[y + 1, x] = pass;
+                                    maze[y + 2, x] = pass;
+                                    y += 2;
+                                }
+                            }
+                            break;
+
+                        case 2: // Налево
+                            if (x != 1)
+                            {
+                                if (maze[y, x - 2] == wall)
+                                {
+                                    maze[y, x - 1] = pass;
+                                    maze[y, x - 2] = pass;
+                                    x -= 2;
+                                }
+                            }
+                            break;
+
+                        case 3: // Направо
+                            if (x != width - 2)
+                            {
+                                if (maze[y, x + 2] == wall)
+                                {
+                                    maze[y, x + 1] = pass;
+                                    maze[y, x + 2] = pass;
+                                    x += 2;
+                                }
+                            }
+                            break;
+                    }
+                    if (deadend(x, y, maze, height, width, pass, wall))
+                    {
+                        break;
+                    }
+                }
+
+                if (deadend(x, y, maze, height, width, pass, wall)) // Вытаскиваем крота из тупика
+                {
+                    do
+                    {
+                        x = 2 * (r.Next() % ((width - 1) / 2)) + 1;
+                        y = 2 * (r.Next() % ((height - 1) / 2)) + 1;
+                    } while (maze[y, x] != pass);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            //Main();
+            Random r = new Random();
+            height = Convert.ToInt32(textBox1.Text);
+            if (height % 2 == 0 & height != 0)
+            {
+                height++;
+            }
+            else if (height <= 0)
+            {
+                MessageBox.Show("Введенное значение должно быть числом и числом больше 0.");
+                return;
+            }
+            width = Convert.ToInt32(textBox2.Text);
+            if (width % 2 == 0 & width != 0)
+            {
+                width++;
+            }
+            else if (width <= 0)
+            {
+                MessageBox.Show("Введенное значение должно быть числом и числом больше 0.");
+                return;
+            }
+
+            begin = true;
+            maze = new int[height, width];
+
+            mazemake(maze, height, width, r, pass, wall);
+
+          //  panel1.Size = new Size(width * side, height * side);
+
+            
+            panel1.Invalidate();
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            if (begin == true) {
+                progressBar1.Value = 0;
+                //double progVal = (width * height) / 100;
+                //int step = Convert.ToInt16(Math.Round(progVal));
+                progressBar1.Minimum = 0;
+                progressBar1.Maximum = width;
+                for (int i = 0; i < width; i++)
+                {
+                    for (int j = 0; j < height; j++)
+                    {
+                        switch (maze[j, i])
+                        {
+                            case 0:
+                                //Console.Write("# ");
+                                DrawBox(i, j, Brushes.Black, e.Graphics);
+                                break;
+                            case 1:
+                                // Console.Write("  ");
+                                //DrawBox(i, j, Brushes.White, e.Graphics);
+                                break;
+                        }
+
+
+
+
+                    }
+                    progressBar1.Value += 1;
+                    //Console.Write("\n");
+                }
+                begin = false;
+            }
+            else
+            {
+                return;
+            }
+            
+           // Console.Write("\n");
+        }
+
+
+    }
+
+}
+
+
